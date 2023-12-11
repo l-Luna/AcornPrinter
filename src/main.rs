dece77a829bb687449556c33d44ba9b5d780f1a9
l-Luna <izzeldeen03@gmail.com>
2023-12-11 16:43:26 +0000

    bonus: arbitrary size & offset

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,21 @@
+use std::env::args;
 use std::fs;
 use std::path::Path;
 
 fn main(){
-    let w = 512;
-    let h = 512;
+    let cmds: Vec<String> = args().collect();
+    let w: usize = str::parse(&*cmds[1]).unwrap();
+    let h: usize = str::parse(&*cmds[2]).unwrap();
+    let offset: usize = str::parse(&*cmds[3]).unwrap();
     let mut values = vec![vec![0; w]; h];
 
-    values[200 + 1][200 + 0] = 255;
-    values[200 + 3][200 + 1] = 255;
-    values[200 + 0][200 + 2] = 255;
-    values[200 + 1][200 + 2] = 255;
-    values[200 + 4][200 + 2] = 255;
-    values[200 + 5][200 + 2] = 255;
-    values[200 + 6][200 + 2] = 255;
+    values[offset + 1][offset + 0] = 255;
+    values[offset + 3][offset + 1] = 255;
+    values[offset + 0][offset + 2] = 255;
+    values[offset + 1][offset + 2] = 255;
+    values[offset + 4][offset + 2] = 255;
+    values[offset + 5][offset + 2] = 255;
+    values[offset + 6][offset + 2] = 255;
 
     fs::write(Path::new("./out.pgm"), write(w, h, values)).unwrap();
 }
